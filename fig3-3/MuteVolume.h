2dbe3a0f8979b44c155cc266d7ce7baea43f6120
delphinus35 <delphinus@remora.cx>
2012-05-19 17:21:49 +0900

    fig3-3 version 2

--- fig3-3/MuteVolume.h
+++ fig3-3/MuteVolume.h
@@ -1,6 +1,12 @@
 #import "Volume.h"
 
 @interface MuteVolume : Volume
+{
+    BOOL muting;
+}
+
+- (id)initWithMin: (int)a max: (int)b step: (int)s; // override
+- (int)value; // override
 - (id)mute;
 @end
 
--- fig3-3/MuteVolume.m
+++ fig3-3/MuteVolume.m
@@ -1,8 +1,18 @@
 #import "MuteVolume.h"
 
 @implementation MuteVolume
+- (id)initWithMin: (int)a max: (int)b step: (int)s // override
+{
+    if ((self = [super initWithMin:a max:b step:s]) != nil) {
+        muting = NO;
+    }
+    return self;
+}
+
+- (int)value { return muting ? min : val; }
+
 - (id)mute {
-    val = min;
+    muting = !muting;
     return self;
 }
 @end
